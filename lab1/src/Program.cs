e7ef024408003f6264367514b57ba538e918923c
magnojezz <magnojezz@ubuntu.netis.cc>
2026-05-02 06:27:19 +0000

    fix: add UseSystemd and skip UseUrls for socket activation

--- lab1/src/Program.cs
+++ lab1/src/Program.cs
@@ -19,7 +19,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, new MariaDbServerVersion(new Version(10, 11))));
 
 builder.Host.UseSystemd();
-builder.WebHost.UseUrls($"http://{appHost}:{appPort}");
+
+if (Environment.GetEnvironmentVariable("LISTEN_FDS") == null)
+{
+    builder.WebHost.UseUrls($"http://{appHost}:{appPort}");
+}
 
 var app = builder.Build();
 
